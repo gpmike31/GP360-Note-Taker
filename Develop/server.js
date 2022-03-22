aadea4268e41d3f7589390df02926f190773265f
gpmike31 <gpmike3105@gmail.com>
2022-03-21 19:53:51 -0500

    updated file paths on app.get to connect back end server and display notes any index and notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const Notes = require("./db/db.json");
+const notes = require("./db/db.json");
 
 const PORT = 3001;
 
@@ -10,22 +10,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "notes.html"));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get("/api/notes", (req, res) => {
-  res.json(Notes);
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
 });
 
-app.post("/api/notes", (req, res) => {
+app.post('/api/notes', (req, res) => {
   // Inform the client that their POST request was received
   res.json(`${req.method} request received to add a new note`);
 
